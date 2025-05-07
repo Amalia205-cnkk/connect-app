547497c91078fb53f617e993eb3681c764da5059
Amalia205-cnkk <lailatulamalia323@gmail.com>
2025-05-07 12:31:51 +0800

    fent create user page

--- app/users/page.js
+++ app/users/page.js
@@ -1,7 +1,19 @@
-export default function UserPage() {
-    return (
-        <div>
-            <p>Halaman User</p>
+export default function UserPage(){
+    return(
+        <div id="conteiner">
+            <section id="navigation">
+                <h1>connect</h1>
+                <div>
+                    <button>Users</button>
+                    <button>Hak Akses</button>
+                    <button>Logout</button>
+                </div>
+            </section>
+            <section id="content">
+                <input type="text" placeholder="Cari Users" />
+                <div id="List-users"></div>
+            </section>
         </div>
-    );
+    )       
 }
+
